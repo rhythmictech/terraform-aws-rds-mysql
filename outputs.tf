675cbcee944148fdf6b8ca063f77c4028f81670a
Steven B <genseb7@gmail.com>
2024-03-19 17:35:20 -0400

    Update outputs.tf (#14)

--- outputs.tf
+++ outputs.tf
@@ -15,8 +15,8 @@ output "instance_connection_info" {
 }
 
 output "instance_id" {
-  description = "Instance ID of RDS DB"
-  value       = aws_db_instance.this.id
+  description = "Instance `identifier` NOT `id` of RDS DB. For backwards compatibility. See [change](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-5-upgrade#aws_db_instanceid-is-no-longer-the-identifier)"
+  value       = aws_db_instance.this.identifier
 }
 output "password_secret_arn" {
   description = "Password secret ARN"
